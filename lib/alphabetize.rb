e713e888f640ddf75b253f53446ddca2ece959a5
Weijun Huang <wjhuang0726@gmail.com>
2018-12-21 00:06:10 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,12 +5,12 @@ new_array=[]
   # code here
   str ="abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-arr.each do |i|
-  str.each_char do |char|
-    if i[0] == char
-      hash[i]=str.index(char)
-    end
-  end
+  arr.each do |i|
+    str.each_char do |char|
+      if i[0] == char
+       hash[i]=str.index(char)
+      end
+   end
 end
 hash.sort_by {|k, v| v}. each do |item|
 new_array.push(item[0])
