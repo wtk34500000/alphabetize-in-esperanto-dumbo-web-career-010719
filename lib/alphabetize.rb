0bb303adb8136d54a971feae2b349648a4ea6a17
Weijun Huang <wjhuang0726@gmail.com>
2018-12-21 00:27:51 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,26 +1,22 @@
 
 def alphabetize(arr)
- # hash={}
- # new_array=[]
-  # code here
+  hash={}
+  new_array=[]
   str ="abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-  if arr.find_all do |item| 
-    if item.start_with?(item[0]) 
-      arr.sort 
+   if arr.all? {|item| item.start_with?(arr.first[0])}
+    arry.sort
+  else
+    arr.each do |i|
+    str.each_char do |char|
+      if i[0] == char
+       hash[i]=str.index(char)
+      end
     end
   end
-    
- # arr.each do |i|
-  #  str.each_char do |char|
-   #   if i[0] == char
-  #     hash[i]=str.index(char)
-   #   end
-  #  end
- # end
- # hash.sort_by {|k, v| v}. each do |item|
-  #  new_array.push(item[0])
- # end
- # new_array
-  
+  hash.sort_by {|k, v| v}. each do |item|
+    new_array.push(item[0])
+  end
+    new_array
+    end
 end
