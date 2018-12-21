842ce6e13c733899a977b079abc702f953e4d5c0
Weijun Huang <wjhuang0726@gmail.com>
2018-12-21 00:01:33 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,19 @@
 
 def alphabetize(arr)
+hash={}
+new_array=[]
   # code here
-  arr.sort
+  str ="abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+arr.each do |i|
+  str.each_char do |char|
+    if i[0] == char
+      hash[i]=str.index(char)
+    end
+  end
+end
+hash.sort_by {|k, v| v}. each do |item|
+new_array.push(item[0])
+end
+new_array
 end
